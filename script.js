a89b71bb2c1ba60a037539619d7b1b0c478095a0
ejiseok <jiseoklee0502@gmail.com>
2026-04-03 23:21:22 +0900

    코드 정리

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// const Player = function(symbol, name) {
-//   const _symbol = symbol;
-//   const _name = name;
-
-//   const getSymbol = function() {
-//     return _symbol;
-//   }
-
-//   const getName = function() {
-//     return _name;
-//   }
-
-//   return {
-//     getSymbol,
-//     getName
-//   };
-// };
-
 class Player {
   #symbol;
   #name;
@@ -165,111 +147,6 @@ class Game {
   }
 }
 
-const game = (function() {
-  const player1 = null;
-  const player2 = null;
-  const board = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-  ];
-
-  let currentTurnPlayer = player1;
-  let playerWhoWin = null;
-
-  const setPlayer1 = function(symbol, name) {
-    player1 = new Player(symbol, name);
-  }
-
-  const setPlayer2 = function(symbol, name) {
-    player2 = new Player(symbol, name);
-  }
-
-  const setPlayerWhowin = function(player) {
-    playerWhoWin = player;
-  }
-
-  const markSymbol = function(pos, cell) {
-    if (board[pos[0]][pos[1]] !== null) {
-      return;
-    }
-
-    board[pos[0]][pos[1]] = currentTurnPlayer.getSymbol();
-    cell.textContent = `${currentTurnPlayer.getSymbol()}`;
-    checkGameEnd();
-    currentTurnPlayer = currentTurnPlayer === player1 ? player2 : player1;
-  }
-
-  const checkWinner = function(symbol) {
-    if (symbol === player1.getSymbol()) {
-      return player1;
-    }
-    return player2;
-  };
-
-  const checkGameEnd = function() {
-    // 가로, 세로 검사
-    for (let i = 0; i < board.length; i++) {
-      if ((board[i][0] !== null && board[i][1] !== null && board[i][2] !== null) &&
-          (board[i][0] === board[i][1] && board[i][1] === board[i][2])) {
-        setPlayerWhowin(checkWinner(board[i][0]));
-        finalResult.textContent = `${playerWhoWin} 승리!`;
-        return;
-      }
-
-      if ((board[0][i] !== null && board[1][i] !== null && board[2][i] !== null) &&
-          (board[0][i] === board[1][i] && board[1][i] === board[2][i])) {
-        setPlayerWhowin(checkWinner(board[0][i]));
-        finalResult.textContent = `${playerWhoWin} 승리!`;
-        return;
-      }
-    }
-
-    // 대각선 검사
-    if ((board[0][0] !== null && board[1][1] !== null && board[2][2] !== null) &&
-        (board[0][0] === board[1][1] && board[1][1] === board[2][2])) {
-      setPlayerWhowin(checkWinner(board[0][0]));
-      finalResult.textContent = `${playerWhoWin} 승리!`;
-        return;
-    }
-
-    if ((board[0][3] !== null && board[1][1] !== null && board[2][0] !== null) &&
-        (board[0][3] === board[1][1] && board[1][1] === board[2][0])) {
-      setPlayerWhowin(checkWinner(board[0][3]));
-      finalResult.textContent = `${playerWhoWin} 승리!`;
-      return;
-    }
-
-    // 무승부 체크, 한 개의 칸이라도 null이면 게임 끝 아님
-    for (let i = 0; i < board.length; i++) {
-      for (let j = 0; j < board[i].length; j++) {
-        if (board[i][j] === null) {
-          return;
-        }
-      }
-    }
-
-    // 무승부
-    finalResult.textContent = "무승부";
-  };
-
-  return {
-    markSymbol,
-    setPlayer1,
-    setPlayer2
-  };
-})();
-
-// const player1Info = document.querySelector(".player-1");
-// const player2Info = document.querySelector(".player-2");
-// const finalResult = document.querySelector(".final-result");
-
-// for (let i = 0; i < 9; i++) {
-//   const cell = document.querySelector(`.cell-${i}`);
-//   const pos = [parseInt(i / 3), i % 3];
-//   cell.addEventListener("click", () => game.markSymbol(pos, cell));
-// }
-
 const player1Name = prompt("플레이어 1 이름 입력");
 const player2Name = prompt("플레이어 2 이름 입력");
 
@@ -277,6 +154,3 @@ const viewUpdater = new ViewUpdater();
 
 viewUpdater.setPlayer1InfoContent(`${player1Name} -> O`);
 viewUpdater.setPlayer2InfoContent(`${player2Name} -> X`);
-
-// game.setPlayer1("O", player1Name);
-// game.setPlayer2("X", player2Name);
