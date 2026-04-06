4a83a2c04b60cd98c90abe5348696620d6ddde4e
ejiseok <jiseoklee0502@gmail.com>
2026-04-06 12:11:12 +0900

    클래스 구조 변경 진행 중

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
+class Cell {
+  #mark;
+
+  constructor() {
+    this.#mark = null;
+  }
+
+  isEmpty() {
+    if (this.#mark === null) {
+      return true;
+    }
+    return false;
+  }
+
+  setMark(mark) {
+    this.#mark = mark;
+  }
+
+  getMark() {
+    return this.#mark;
+  }
+}
+
 class Player {
-  #symbol;
+  #mark;
   #name;
 
-  constructor(symbol, name) {
-    this.#symbol = symbol;
+  constructor(mark, name) {
+    this.#mark = mark;
     this.#name = name;
   }
 
-  getSymbol() {
-    return this.#symbol;
+  makeMove(board) {
+    // TODO
+  }
+
+  getMark() {
+    return this.#mark;
   }
 
   getName() {
@@ -16,146 +43,111 @@ class Player {
   }
 }
 
-class ViewUpdater {
-  #player1Info;
-  #player2Info;
-  #finalResult;
-  #cells = [[null, null, null], [null, null, null], [null, null, null]];
+class Board {
+  #grid;
 
-  #game;
-
-  constructor(game) {
-    this.#player1Info = document.querySelector(".player-1");
-    this.#player2Info = document.querySelector(".player-2");
-    this.#finalResult = document.querySelector(".final-result");
-
-    for (let i = 0; i < 9; i++) {
-      const cell = document.querySelector(`.cell-${i}`);
-      this.#cells[parseInt(i / 3)][i % 3] = cell;
-    }
-
-    this.#game = game;
-  }
-
-  setPlayer1InfoContent(text) {
-    this.#player1Info.textContent = text;
-  }
-
-  setPlayer2InfoContent(text) {
-    this.#player2Info.textContent = text;
-  }
-
-  setFinalResultContent(text) {
-    this.#finalResult.textContent = text;
-  }
-
-  drawSymbolCell(symbol, pos) {
-    this.#cells[pos[0]][pos[1]].textContent = symbol;
-  }
-
-  addHandlerToEachCell() {
-    for (let i = 0; i < this.#cells.length; i++) {
-      for (let j = 0; j < this.#cells[i].length; j++) {
-
-      }
-    }
-  }
-}
-
-class Game {
-  #player1;
-  #player2;
-  #currentTurnPlayer;
-  #playerWhoWin;
-  #board;
-  #viewUpdater;
-
-  constructor(player1, player2, viewUpdater) {
-    this.#player1 = player1;
-    this.#player2 = player2;
-    this.#viewUpdater = viewUpdater;
-    this.#currentTurnPlayer = this.#player1;
-    this.#board = [
-      [null, null, null],
-      [null, null, null],
-      [null, null, null]
+  constructor() {
+    this.#grid = [
+      [new Cell(), new Cell(), new Cell()],
+      [new Cell(), new Cell(), new Cell()],
+      [new Cell(), new Cell(), new Cell()]
     ];
   }
 
-  #setPlayerWhoWin(player) {
-    this.#playerWhoWin = player;
-  }
-
-  #switchPlayerTurn() {
-    if (this.#currentTurnPlayer === this.#player1) {
-      this.#currentTurnPlayer = this.#player2;
-    } else {
-      this.#currentTurnPlayer = this.#player1;
+  initialize() {
+    for (let i = 0; i < this.#grid; i++) {
+      for (let j = 0; j < this.#grid[i].length; j++) {
+        this.#grid[i][j].setMark(null);
+      }
     }
   }
 
-  #checkWinner(symbol, isDraw) {
-    if (isDraw) {
-      viewUpdater.setFinalResultContent("무승부");
+  display() {
+    // TODO
+  }
+
+  placeMark(row, col, mark) {
+    if (this.#grid[row][col] !== null) {
       return;
     }
+    this.#grid[row][col].setMark(mark);
+  }
 
-    if (symbol === this.#player1.getSymbol()) {
-      this.#setPlayerWhoWin(this.#player1);
-    } else {
-      this.#setPlayerWhoWin(this.#player2);
-    }
-
-    viewUpdater.setFinalResultContent(`${this.#playerWhoWin} 승리!`);
-  };
-
-  #checkGameEnd() {
+  checkWin(mark) {
     // 가로, 세로 검사
-    for (let i = 0; i < this.#board.length; i++) {
-      if ((this.#board[i][0] !== null && this.#board[i][1] !== null && this.#board[i][2] !== null) &&
-          (this.#board[i][0] === this.#board[i][1] && this.#board[i][1] === this.#board[i][2])) {
-        return this.#currentTurnPlayer;
+    for (let i = 0; i < this.#grid.length; i++) {
+      if ((this.#grid[i][0] !== null && this.#grid[i][1] !== null && this.#grid[i][2] !== null) &&
+          (this.#grid[i][0] === mark) &&
+          (this.#grid[i][1] === mark) &&
+          (this.#grid[i][2] === mark)) {
+        return true;
       }
 
-      if ((this.#board[0][i] !== null && this.#board[1][i] !== null && this.#board[2][i] !== null) &&
-          (this.#board[0][i] === this.#board[1][i] && this.#board[1][i] === this.#board[2][i])) {
-        return this.#currentTurnPlayer;
+      if ((this.#grid[0][i] !== null && this.#grid[1][i] !== null && this.#grid[2][i] !== null) &&
+          (this.#grid[0][i] === mark) &&
+          (this.#grid[1][i] === mark) &&
+          (this.#grid[2][i] === mark)) {
+        return true;
       }
     }
 
     // 대각선 검사
-    if ((this.#board[0][0] !== null && this.#board[1][1] !== null && this.#board[2][2] !== null) &&
-        (this.#board[0][0] === this.#board[1][1] && this.#board[1][1] === this.#board[2][2])) {
-      return this.#currentTurnPlayer;
+    if ((this.#grid[0][0] !== null && this.#grid[1][1] !== null && this.#grid[2][2] !== null) &&
+        (this.#grid[0][0] === mark) &&
+        (this.#grid[1][1] === mark) &&
+        (this.#grid[2][2] === mark)) {
+      return true;
     }
 
-    if ((this.#board[0][3] !== null && this.#board[1][1] !== null && this.#board[2][0] !== null) &&
-        (this.#board[0][3] === this.#board[1][1] && this.#board[1][1] === this.#board[2][0])) {
-      return this.#currentTurnPlayer;
+    if ((this.#grid[0][2] !== null && this.#grid[1][1] !== null && this.#grid[2][0] !== null) &&
+        (this.#grid[0][2] === mark) &&
+        (this.#grid[1][1] === mark) &&
+        (this.#grid[2][0] === mark)) {
+      return true;
     }
+    
+    return false;
+  }
 
-    // 무승부 체크, 한 개의 칸이라도 null이면 게임 끝 아님
-    for (let i = 0; i < this.#board.length; i++) {
-      for (let j = 0; j < this.#board[i].length; j++) {
-        if (this.#board[i][j] === null) {
-          return "NOT_OVER";
+  isFull() {
+    for (let i = 0; i < this.#grid.length; i++) {
+      for (let j = 0; j < this.#grid[i].length; j++) {
+        if (this.#grid[i][j] === null) {
+          return false;
         }
       }
     }
+    return true;
+  }
+}
 
-    // 무승부
-    return "DRAW";
+class Game {
+  #board;
+  #player1;
+  #player2;
+  #currentPlayer;
+
+  constructor(board, player1, player2) {
+    this.#board = board;
+    this.#player1 = player1;
+    this.#player2 = player2;
+    this.#currentPlayer = this.#player1;
   }
 
-  #markSymbol(pos) {
-    if (this.#board[pos[0]][pos[1]] !== null) {
-      return;
-    }
+  start() {
+    // TODO
+  }
 
-    this.#board[pos[0]][pos[1]] = this.#currentTurnPlayer.getSymbol();
-    viewUpdater.drawSymbolCell(pos, this.#currentTurnPlayer.getSymbol());
-    this.#checkGameEnd();
-    this.#switchPlayerTurn();
+  switchPlayer() {
+    if (this.#currentPlayer === this.#player1) {
+      this.#currentPlayer = this.#player2;
+    } else {
+      this.#currentPlayer = this.#player1;
+    }
+  }
+
+  announceResult() {
+    // TODO
   }
 }
 
